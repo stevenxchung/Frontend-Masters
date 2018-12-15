17f1c643cc981f2d79757b4438e48478c39b5f37
Steven <stevenxchung@gmail.com>
2018-12-15 11:42:06 -0500

    Code readability arrow functions vs classical functions

---
+++ ES6 - The Right Parts - Kyle Simpson/01-arrow-functions.js
@@ -0,0 +1,8 @@
+// Functions vs arrow functions
+// Some things to keep in mind: is it easier to read the arrow function or the classical function?
+// The arrow function seems simple, shorter, more intuitive at first
+foo = x => 2;
+// The classical function might be more readible to most users vs the arrow function
+function foo() {
+  return 2;
+}
