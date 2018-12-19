980e9fdf1e845e0926f002f6cd6fa89b33034a5f
Steven <stevenxchung@gmail.com>
2018-12-18 21:50:32 -0500

    First attempt

--- ES6 - The Right Parts - Kyle Simpson/01-ex0.js
+++ ES6 - The Right Parts - Kyle Simpson/01-ex0.js
@@ -18,7 +18,6 @@
           }.bind(this),
           100
         );
-
         return obj;
       }
     };
@@ -35,6 +34,55 @@
     console.log(
       list1[0] ===
         list2.reduce(function(s, v) {
+          return s + v;
+        }, 0)
+    );
+  }, 200);
+})();
+
+// ==================================================
+
+(IIFE = () => {
+  let foo = x => {
+    let y = x * 2;
+    // Return bar(z)
+    return (bar = z => {
+      if (z.length > 3) {
+        // Return baz(v)
+        return z.map(
+          (baz = v => {
+            if (v > 3) {
+              return v + y;
+            } else {
+              return baz(v * 4);
+            }
+          })
+        );
+      } else {
+        let obj = [];
+        setTimeout(
+          (bam = () => {
+            obj.length = 1;
+            obj[0] = this.w;
+          }),
+          100
+        );
+        return obj;
+      }
+    });
+  };
+
+  let p = foo(2);
+  let list1 = [1, 3, 4];
+  let list2 = list1.concat(6);
+
+  list1 = p.call({ w: 42 }, list1);
+  list2 = p(list2);
+
+  setTimeout(() => {
+    console.log(
+      list1[0] ===
+        list2.reduce((s, v) => {
           return s + v;
         }, 0)
     );
