0a6f5ab608347fb8ca99d7ddb52f1aa45e36b04b
Steven <stevenxchung@gmail.com>
2018-12-20 20:04:37 -0500

    Add block scoping example with var and let

---
+++ ES6 - The Right Parts - Kyle Simpson/02-block-scope.js
@@ -0,0 +1,25 @@
+// Block scoping - let vs var
+function foo(x, y) {
+  if (x < y) {
+    // Here var attaches tmp to the scope of foo() not to the if statement
+    var tmp = x;
+    x = y;
+    y = tmp;
+  }
+  // Here we signal to the reader that the variable i belongs to the for loop
+  // However you can still use i outside of the for loop
+  for (var i = 0; i < 10; i++) {}
+}
+
+// Using let we can actually limit the variables to their scope
+function foo(x, y) {
+  if (x < y) {
+    // Here let attaches tmp to the scope of the if statement
+    let tmp = x;
+    x = y;
+    y = tmp;
+  }
+  // Here we signal to the reader that i actually belongs to the scope of the for loop
+  // The variable i cannot be used outside of the loop
+  for (let i = 0; i < 10; i++) {}
+}
