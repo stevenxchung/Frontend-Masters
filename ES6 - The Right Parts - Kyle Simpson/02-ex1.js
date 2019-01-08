fb84a3b705673888029692f2b7117579ee61be14
Steven <stevenxchung@gmail.com>
2019-01-07 19:07:53 -0500

    Add attempt

--- ES6 - The Right Parts - Kyle Simpson/02-ex1.js
+++ ES6 - The Right Parts - Kyle Simpson/02-ex1.js
@@ -12,3 +12,24 @@ var x = 2,
 })();
 
 console.log(x * 2 === fns[x * 2]()); // True
+
+// ==================================================
+
+// Initial attempt
+var x = 2,
+  fns = [];
+
+(function() {
+  var x = 5;
+
+  for (var i = 0; i < x; i++) {
+    fns[i] = function num() {
+      return i - 1;
+    }
+  }
+})();
+
+console.log('x * 2 = ', x * 2); // 4
+console.log('fns[x * 2]() = ', fns[x * 2]()); // 4
+
+console.log(x * 2 === fns[x * 2]()); // True
