025da340c73135d4361d6293bcc759b83998444c
Steven <stevenxchung@gmail.com>
2019-01-08 19:24:46 -0500

    Add rewrite for readability

--- ES6 - The Right Parts - Kyle Simpson/03-gather-spread.js
+++ ES6 - The Right Parts - Kyle Simpson/03-gather-spread.js
@@ -1,6 +1,4 @@
 // Default values
-
-// Before
 // Given the function below
 function foo(x) {
   // If x is truthy, return x, else return 42
@@ -10,5 +8,12 @@ function foo(x) {
 
 // What happens if we pass in zero?
 foo(0) // 42, since 0 is falsey
+
+// How can we rewrite this to be more readable?
+function foo(x) {
+  // Here it is easy to see that if value is not undefined, return x
+  x = x !== undefined ? x : 42;
+  return x;
+}
 
 // ==================================================
