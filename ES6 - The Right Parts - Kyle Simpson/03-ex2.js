1a2b348b132ea9805972fd8811a0680cb68277cb
Steven <stevenxchung@gmail.com>
2019-01-13 14:03:35 -0500

    Exercise 2 initial commit

---
+++ ES6 - The Right Parts - Kyle Simpson/03-ex2.js
@@ -0,0 +1,14 @@
+// Exercise 2
+// Use gather and spreads to return the following
+function foo() {}
+
+function bar() {
+  var a1 = [2, 4];
+  var a2 = [6, 8, 10, 12];
+
+  return foo();
+}
+
+console.log(bar().join('') === '281012');
+
+// ==================================================
