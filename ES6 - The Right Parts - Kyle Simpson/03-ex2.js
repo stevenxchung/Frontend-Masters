a89e454722b0ee4df370cf217ad85058fb7b3574
Steven <stevenxchung@gmail.com>
2019-01-13 14:23:05 -0500

    Add solution

--- ES6 - The Right Parts - Kyle Simpson/03-ex2.js
+++ ES6 - The Right Parts - Kyle Simpson/03-ex2.js
@@ -28,3 +28,22 @@ function bar() {
 // Log output
 console.log(bar().join(''));
 console.log(bar().join('') === '281012');
+
+// ==================================================
+
+// Solution
+// By only using x and ..rest we are effectively ignoring the 2nd and 3rd numbers
+function foo(x, y, z, ...rest) {
+  return [x, ...rest];
+}
+
+function bar() {
+  var a1 = [2, 4];
+  var a2 = [6, 8, 10, 12];
+
+  return foo(...a1, ...a2);
+}
+
+// Log output
+console.log(bar().join(''));
+console.log(bar().join('') === '281012');
