4a8f5ef9ee784be2b4d52d959c39cbc585cb87fe
Steven <stevenxchung@gmail.com>
2019-01-23 18:45:09 -0500

    Add ES6 concise properties example

--- ES6 - The Right Parts - Kyle Simpson/05-template-strings.js
+++ ES6 - The Right Parts - Kyle Simpson/05-template-strings.js
@@ -4,4 +4,9 @@ var a = 1;
 // Before ES6 you would have to declare the following for 'a'
 var obj = {
   a: a
-}
+};
+// In ES6, if the property name and value are the same we can declare 'a' to be
+var obj = {
+  // This is by using concise properties
+  a
+};
