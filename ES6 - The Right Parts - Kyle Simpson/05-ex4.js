61ae6329a137e458aa88118dc3df0fcd9af8fb0c
Steven <stevenxchung@gmail.com>
2019-01-26 11:16:44 -0500

    Exercise 4 initial commit

---
+++ ES6 - The Right Parts - Kyle Simpson/05-ex4.js
@@ -0,0 +1,12 @@
+// Exercise 4
+// Use tag function and string interpolation template to get console.log() to return true
+function upper(strings, ...values) {}
+
+var name = 'Steven',
+  twitter = 'getify',
+  classname = 'es6 workshop';
+
+console.log(
+  `Hello ____ (@____), welcome to the ____!` ===
+    'Hello STEVEN (@GETIFY), welcome to the ES6 WORKSHOP!'
+);
