4d885fa1326b6e8c9d1acae94fbc5f15d7dfff11
Steven <stevenxchung@gmail.com>
2019-01-26 11:57:49 -0500

    Add initial attempt

--- ES6 - The Right Parts - Kyle Simpson/05-ex4.js
+++ ES6 - The Right Parts - Kyle Simpson/05-ex4.js
@@ -10,3 +10,26 @@ console.log(
   `Hello ____ (@____), welcome to the ____!` ===
     'Hello STEVEN (@GETIFY), welcome to the ES6 WORKSHOP!'
 );
+
+// ==================================================
+
+// Initial attempt
+function upper(strings, ...values) {
+  var str = '';
+  for (var i = 0; i < strings.length - 1; i++) {
+    str += strings[i] + values[i].toUpperCase();
+  }
+  str += strings[strings.length - 1];
+  return str;
+}
+
+var name = 'Steven',
+  twitter = 'getify',
+  className = 'es6 workshop';
+
+console.log(upper`Hello ${name} (@${twitter}), welcome to the ${className}!`);
+
+console.log(
+  upper`Hello ${name} (@${twitter}), welcome to the ${className}!` ===
+    'Hello STEVEN (@GETIFY), welcome to the ES6 WORKSHOP!'
+);
