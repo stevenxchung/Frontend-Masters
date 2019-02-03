81dd4a6c28614bcfa67b7dfea578782b125c62c2
Steven <stevenxchung@gmail.com>
2019-02-03 09:04:18 -0500

    Exercise 5 initial commit

---
+++ ES6 - The Right Parts - Kyle Simpson/06-ex5.js
@@ -0,0 +1,14 @@
+// Exercise 5
+var numbers = {
+  // ...
+};
+
+// Should print 0 to 100 by 1s
+for (let num of numbers) {
+  console.log(num);
+}
+
+// Should print 6 to 30 by 4s
+for (let num of x /* <insert array here> */) {
+  console.log(num);
+}
