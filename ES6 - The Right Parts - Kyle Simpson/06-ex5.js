3dc4c693bf014ed6a473767e8cd67c99b643d58b
Steven <stevenxchung@gmail.com>
2019-02-04 18:49:41 -0500

    Initial attempt - programmable generator

--- ES6 - The Right Parts - Kyle Simpson/06-ex5.js
+++ ES6 - The Right Parts - Kyle Simpson/06-ex5.js
@@ -9,6 +9,37 @@ for (let num of numbers) {
 }
 
 // Should print 6 to 30 by 4s
-for (let num of x /* <insert array here> */) {
+for (let num of /* <insert array here> */) {
   console.log(num);
 }
+
+// ==================================================
+
+// Initial attempt
+var numbers = {
+  arrayGen: function* (start = 0, end = 100) {
+    for (var i = start; i <= end; i++) {
+      yield i;
+    }
+  },
+};
+
+// Should print 0 to 100 by 1s
+for (let num of numbers.arrayGen()) {
+  console.log(num);
+}
+
+// Should print 6 to 30 by 4s
+for (let num of numbers.arrayGen(start = 6, end = 30)) {
+  console.log(num);
+}
+
+// let arrayGen = (start, end) => {
+//   let arr = [];
+//   for (var i = start; i <= end; i++) {
+//     arr.push(i);
+//   };
+//   return arr;
+// }
+
+// console.log(arrayGen(1, 5))
