34d97f23d9c56a6478b7a2d46b84285f8b5e3297
Steven <stevenxchung@gmail.com>
2019-02-04 19:00:25 -0500

    Fix problem statement wording (weird unit s) and adjust attempt

--- ES6 - The Right Parts - Kyle Simpson/06-ex5.js
+++ ES6 - The Right Parts - Kyle Simpson/06-ex5.js
@@ -3,33 +3,33 @@ var numbers = {
   // ...
 };
 
-// Should print 0 to 100 by 1s
+// Should print 0 to 100 by 1 step
 for (let num of numbers) {
   console.log(num);
 }
 
-// Should print 6 to 30 by 4s
-for (let num of /* <insert array here> */) {
-  console.log(num);
-}
+// Should print 6 to 30 in increments of 4
+// for (let num of /* <insert array here> */) {
+//   console.log(num);
+// }
 
 // ==================================================
 
 // Initial attempt
 var numbers = {
-  arrayGen: function* (start = 0, end = 100) {
-    for (var i = start; i <= end; i++) {
+  arrayGen: function*(start = 0, end = 100, step = 1) {
+    for (var i = start; i <= end; i += step) {
       yield i;
     }
-  },
+  }
 };
 
-// Should print 0 to 100 by 1s
+// Should print 0 to 100 by 1 step
 for (let num of numbers.arrayGen()) {
   console.log(num);
 }
 
-// Should print 6 to 30 by 4s
-for (let num of numbers.arrayGen(start = 6, end = 30)) {
+// Should print 6 to 30 in increments of 4
+for (let num of numbers.arrayGen((start = 6), (end = 30), (step = 4))) {
   console.log(num);
 }
